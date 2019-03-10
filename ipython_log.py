1bb3e4e1b2d154765d3f84decda38c62714e1d64
gordontttt <8080call,OF.duty9>
2019-03-10 20:17:34 +1100

    first commit

---
+++ ipython_log.py
@@ -0,0 +1,273 @@
+# IPython log file
+
+import numpy as np
+import pandas as pd
+def exmaple():
+    print("It is an example")
+# ?? will also show the source code
+# ? also could use to search function like UNIX Wildcard
+get_ipython().run_line_magic('psearch', 'np.*load*')
+get_ipython().run_line_magic('pinfo2', 'exmaple')
+# use %run command to run python script file 
+# all global variables, function and import could be used in the notebook
+# add command argument behind when use sys.argv
+get_ipython().run_line_magic('run', 'file.py')
+get_ipython().run_line_magic('matplotlib', 'inline')
+28 * 90
+_
+28 * 90 
+444 * aa
+28 * 90 
+444 * 10
+_
+__
+__
+__
+_
+28 * 90 
+444 * 10
+_14
+_13
+_14
+_
+_ #the latest result will saved into _ variable
+_13 # will show the result in In[13]
+# _ #the latest result will saved into _ variable
+_13 # will show the result in In[13]
+_ #the latest result will saved into _ variable
+_13 # will show the result in In[13]
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+get_ipython().run_line_magic('env', '')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+get_ipython().run_line_magic('pwd', '')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+s
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+get_ipython().run_line_magic('ls', '')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+a = get_ipython().run_line_magic('ls', '')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+a = get_ipython().run_line_magic('ls', '')
+a
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+a = get_ipython().run_line_magic('ls', '')
+type(a)
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+a = get_ipython().run_line_magic('ls', '')
+a[0]
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+a = get_ipython().run_line_magic('ls', '')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+get_ipython().run_line_magic('ls', '')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+get_ipython().run_line_magic('ls', '')
+type(s)
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+get_ipython().run_line_magic('ls', '')
+get_ipython().system('ls')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# %ls
+get_ipython().system('ls')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# %ls
+get_ipython().system('ls *.py')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# %ls
+a = get_ipython().getoutput('ls *.py')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# %ls
+a = get_ipython().getoutput('ls *.py')
+a
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# put ! in front of any shell command and could assgin the result into variables
+a = get_ipython().getoutput('ls ')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# put ! in front of any shell command and could assgin the result into variables
+a = get_ipython().getoutput('ls ')
+a
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# put ! in front of any shell command and could assgin the result into variables
+a = get_ipython().getoutput('ls ')
+# use bookmark to create path quick access
+get_ipython().run_line_magic('bookmark', 'db /home/gordontttt/Documents/')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# put ! in front of any shell command and could assgin the result into variables
+a = get_ipython().getoutput('ls ')
+# use bookmark to create path quick access
+get_ipython().run_line_magic('bookmark', 'db /home/gordontttt/Documents/')
+cd db
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# put ! in front of any shell command and could assgin the result into variables
+a = get_ipython().getoutput('ls ')
+# use bookmark to create path quick access
+get_ipython().run_line_magic('bookmark', 'db /home/gordontttt/Documents/')
+get_ipython().system('cd db')
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# put ! in front of any shell command and could assgin the result into variables
+a = get_ipython().getoutput('ls ')
+# use bookmark to create path quick access
+get_ipython().run_line_magic('bookmark', 'db /home/gordontttt/Documents/')
+db
+def debugExample():
+def debugExample():
+    print('sdada')
+    a = 2
+    b = 1
+    c = 3
+    d = a + b + c
+    print(d)
+get_ipython().run_line_magic('debug', '')
+debugExample
+get_ipython().run_line_magic('debug', '')
+debugExample()
+# use logstart to log
+get_ipython().run_line_magic('logstart', '')
+s = get_ipython().run_line_magic('pwd', '')
+# % could also run shell command like pwd, cd, pushd, popd
+# put ! in front of any shell command and could assgin the result into variables
+a = get_ipython().getoutput('ls ')
+# use bookmark to create path quick access
+get_ipython().run_line_magic('bookmark', 'db /home/gordontttt/Documents/')
+get_ipython().run_line_magic('debug', '')
+debugExample()
+get_ipython().run_line_magic('debug', '')
+debugExample()
+debugExample()
+get_ipython().run_line_magic('debug', '')
+debugExample()
+get_ipython().run_line_magic('debug', '')
+debugExample()
+get_ipython().run_line_magic('debug', '')
+debugExample()
+get_ipython().run_line_magic('debug', '')
+import numpy as np
+import pandas as pd
+def exmaple():
+    print("It is an example")
+# ?? will also show the source code
+# ? also could use to search function like UNIX Wildcard
+get_ipython().run_line_magic('psearch', 'np.*load*')
+get_ipython().run_line_magic('pinfo2', 'exmaple')
+# use %run command to run python script file 
+# all global variables, function and import could be used in the notebook
+# add command argument behind when use sys.argv
+get_ipython().run_line_magic('run', 'file.py')
+# magic command starts with 
+# use %timeit to check the running time of any function and command multiple times
+# %timeit exmaple()
+get_ipython().run_line_magic('quickref', '# use to quick reference magic command')
+get_ipython().run_line_magic('magic', '# use to show all magic command docs')
+get_ipython().run_line_magic('debug', '# use to check the debug information')
+get_ipython().run_line_magic('hist', '# use to check print history')
+get_ipython().run_line_magic('pdb', '# when there is exception, automatically goes into the debug model')
+get_ipython().run_line_magic('time', '# will run the statement once and show the running time')
+get_ipython().run_line_magic('matplotlib', 'inline #show plot inline')
+debugExample()
+get_ipython().run_line_magic('debug', '')
+#create set_trace function and debug function
+def set_trace():
+    from Ipython.core.debugger import Pdb
+    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+
+def debug(f, *args, **kwargs):
+    from IPython.core.debugger import Pdb
+    Pdb(color_scheme='Linux')
+    return Pdb.runcall(f, *args, **kwargs)
+def debugExample():
+    print('sdada')
+    a = 2
+    b = 1
+    c = 3
+    d = a + b + c
+    print(d)
+# use cProfile to check the performance (all functions running time)
+get_ipython().run_line_magic('prun', 'debugExample()')
+# use cProfile to check the performance (all functions running time)
+get_ipython().run_line_magic('prun', 'debugExample() # check the running time of the function')
+# use %lprun to check each line performance of the function
+# command structure %lprun -f func1 -f func2 statement_to_profile
+get_ipython().run_line_magic('lprun', '')
+# use cProfile to check the performance (all functions running time)
+get_ipython().run_line_magic('prun', 'debugExample() # check the running time of the function')
+# use %lprun to check each line performance of the function
+# command structure %lprun -f func1 -f func2 statement_to_profile
+get_ipython().run_line_magic('load_ext', 'line_profiler')
+# use cProfile to check the performance (all functions running time)
+get_ipython().run_line_magic('prun', 'debugExample() # check the running time of the function')
+# use %lprun to check each line performance of the function
+# command structure %lprun -f func1 -f func2 statement_to_profile
+get_ipython().run_line_magic('load_ext', 'line_profiler')
+get_ipython().run_line_magic('lprun', 'debugExample()')
+# use cProfile to check the performance (all functions running time)
+# %prun debugExample() # check the running time of the function
+# use %lprun to check each line performance of the function
+# command structure %lprun -f func1 -f func2 statement_to_profile
+get_ipython().run_line_magic('load_ext', 'line_profiler')
+get_ipython().run_line_magic('lprun', 'debugExample()')
+import this
